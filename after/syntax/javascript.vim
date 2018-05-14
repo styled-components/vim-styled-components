styled-components/vim-styled-components

commit 7c26e3a02084733939aa13f5d79072839d91d6d7

Improve typescript support
See #37 for discussions related to this commit.

- Add rule to include type-annotated function-style prefixes. This is
  done by allowing regions of the form "<(type-annotation)>" between
  `styled` and the function call parentheses.
- Add `typescriptInterpolation` alongside `jsTemplateExpressions` as
  they are virtually the same for the different languages.
- Update `jsNestedRegion` to not include `jsTemplateExpressions`
  (i.e. skip any "${" start patterns). This is necessary to distinguish
  between `cssCustomAttrRegion` and `typescriptInterpolation` regions.
- Add example typescript code to develop against.

after/syntax/javascript.vim
@@ -39,13 +39,15 @@ syn region cssCustomAttrRegion contained
       \ contains=css.*Attr,cssColor,cssImportant,cssValue.*,cssFunction,
       \          cssString.*,cssURL,cssComment,cssUnicodeEscape,cssVendor,
       \          cssError,cssAttrComma,cssNoise,cssPseudoClassId,
-      \          jsTemplateExpression
+      \          jsTemplateExpression,
+      \          typescriptInterpolation
 syn region cssCustomAttrRegion contained
       \ start="transition\s*:" end="\ze\%(;\|)\|{\|}\|`\)"
       \ contains=css.*Prop,css.*Attr,cssColor,cssImportant,cssValue.*,
       \          cssFunction,cssString.*,cssURL,cssComment,cssUnicodeEscape,
       \          cssVendor,cssError,cssAttrComma,cssNoise,cssPseudoClassId,
-      \          jsTemplateExpression
+      \          jsTemplateExpression,
+      \          typescriptInterpolation
 
 " define custom css elements to not utilize cssDefinition
 syn region cssCustomMediaBlock contained fold transparent matchgroup=cssBraces
@@ -104,6 +106,18 @@ syn match styledPrefix "\.\<extend\>"
       \ nextgroup=styledDefinition
       \ containedin=jsFuncBlock
 
+" define custom API section, that contains typescript annotations
+" this is structurally similar to `jsFuncCall`, but allows type
+" annotations (delimited by brackets (e.g. "<>")) between `styled` and
+" the function call parenthesis
+syn match styledTypescriptPrefix
+      \ "\<styled\><\%(\k\|'\|\"\|`\|,\|\s\)\+>(\%('\k\+'\|\"\k\+\"\|`\k\+`\))"
+      \ transparent fold
+      \ nextgroup=styledDefinition
+      \ contains=cssTagName,
+      \          typescriptOpSymbols,typescriptEndColons,typescriptParens,
+      \          styledTagNameString
+
 " define emotion css prop
 " to bypass problems from top-level defined xml/js definitions, this
 " plugin re-defines keywords/noise for highlighting inside of the custom
@@ -122,7 +136,7 @@ syn region styledXmlRegion
 " define nested region for indenting
 syn region styledNestedRegion contained transparent
       \ matchgroup=cssBraces
-      \ start="{" end="}"
+      \ start="[^\$]{" end="}"
 
 " re-define cssError to be highlighted correctly in styledNestedRegion
 syn match cssError contained "{@<>"
@@ -153,6 +167,7 @@ syn region styledDefinition contained transparent fold extend
       \          cssHacks,
       \          cssCustom.*,
       \          jsComment,jsTemplateExpression,
+      \          typescriptInterpolation,
       \          styledAmpersand,styledNestedRegion
 syn region styledDefinitionArgument contained transparent start=+(+ end=+)+
       \ contains=styledDefinition

examples/issue-37.ts
@@ -1,0 +1,33 @@
+import * as React from 'react';
+
+import styled from '@src/lib/styled-components';
+
+interface CardProps {
+  big: boolean;
+}
+
+// This has seemingly correct indenting, but no syntax highlighting.
+export const Card = styled<CardProps, `div`>('div')`
+  background-color: ${props => props.theme.cardBackgroundColor};
+  border-radius: ${props => props.theme.baseBorderRadius};
+  padding: ${props => props.big ?
+                      props.theme.largeMargin : props.theme.normalMargin};
+`;
+
+// This has correct syntax highlighting, but indenting is incorrect
+// (for example, when in insert mode, opening a new line puts cursor at
+// column 0 instead of column 3.)
+export const SomeOtherThing = styled.p`
+  background-color: red;
+`;
+
+// Same as above example (correct highlighting, incorrect indenting)
+const linkStyles = css`
+  font-weight: bold;
+  color: ${props => props.theme.linkColor};
+  text-decoration: underline;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;

commit 1344c3ded87b98e1d67f906fa6f85ed2d3e48959

Fix syntax regression introduced in 7c26e3a
Undo the change to the start-pattern for `styledNestedRegion`, as this
breaks correctly registering beginnings of any nested region in
javascript files.

I guess this change was necessary to make `${` beginnings in typescript
files register as typescriptTemplateInterpolation regions, but I
couldn't reproduce the faulty behavior with the now undone change.

after/syntax/javascript.vim
@@ -136,7 +136,7 @@ syn region styledXmlRegion
 " define nested region for indenting
 syn region styledNestedRegion contained transparent
       \ matchgroup=cssBraces
-      \ start="[^\$]{" end="}"
+      \ start="{" end="}"
 
 " re-define cssError to be highlighted correctly in styledNestedRegion
 syn match cssError contained "{@<>"